f8afb6d7df1fe450d9d70c95b1ca36541a33d632
victoracechen <victoracechen@gmail.com>
2016-07-18 14:44:23 -0400

    routes ES6

--- frontend/velvetRoom.js
+++ frontend/velvetRoom.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
-class VelvetRoom extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Contacts List</h1>
-      </div>
-    )
-  }
+import App from './components/App.js';
+import FrontPage from './components/FrontPage';
 
-}
+  var Routes = (
+    <Router history={ hashHistory }>
+      <Route path="/" component={ App }>
+      <IndexRoute component={ FrontPage }/>
+      </Route>
+    </Router>
+  );
+
 document.addEventListener("DOMContentLoaded", () => {
   var root = document.getElementById('content');
-ReactDOM.render(<VelvetRoom/>, root);
-}); 
+  ReactDOM.render(Routes, root);
+});
